337a07437494222443b25a2792afb34c479642e1
nhan.nguyen <truongnhan0311@gmail.com>
2023-11-07 10:32:50 +0700

    get pdf text

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from dotenv import load_dotenv
 import streamlit as st
 from htmlTemplates import css, bot_template, user_template
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import HuggingFaceHub
 
 
 def handle_userinput(user_question):
@@ -20,8 +27,38 @@ def get_pdf_text(pdf_docs):
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extractText()
+            text += page.extract_text()
     return text
+
+
+def get_text_chunks(text):
+    text_split = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_split.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vector):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
 
 
 def main():
@@ -47,6 +84,9 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing"):
                 raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vector_store = get_vectorstore(text_chunks)
+                st.session_state.conversation = get_conversation_chain(vector_store)
 
 
 if __name__ == '__main__':
